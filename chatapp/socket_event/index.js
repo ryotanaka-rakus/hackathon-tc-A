c2f403d982cfefc250e39f89070277af3eb2d26e
K4-R4 <mabdman512@gmail.com>
2023-10-26 18:45:03 +0900

    メモ、投稿機能の追加

--- chatapp/socket_event/index.js
+++ chatapp/socket_event/index.js
@@ -1,16 +1,19 @@
 export default (io, socket) => {
   // 入室メッセージをクライアントに送信する
   socket.on("enterEvent", (data) => {
+    console.log("入室: " + data)
     socket.broadcast.emit("enterEvent", data)
   })
 
   // 退室メッセージをクライアントに送信する
   socket.on("exitEvent", (data) => {
+    console.log("退室: " + data)
     socket.broadcast.emit("exitEvent", data)
   })
 
   // 投稿メッセージを送信する
   socket.on("publishEvent", (data) => {
+    console.log("投稿: " + data)
     io.sockets.emit("publishEvent", data)
   })
 }
--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -24,8 +24,11 @@ onMounted(() => {
 // #region browser event handler
 // 投稿メッセージをサーバに送信する
 const onPublish = () => {
+    socket.emit("publishEvent", chatContent.value)
+    console.log(userName)
 
   // 入力欄を初期化
+  chatContent.value = ""
 
 }
 
@@ -37,8 +40,10 @@ const onExit = () => {
 // メモを画面上に表示する
 const onMemo = () => {
   // メモの内容を表示
+  chatList.push(chatContent.value)
 
   // 入力欄を初期化
+  chatContent.value = ""
 
 }
 // #endregion
@@ -75,7 +80,7 @@ const registerSocketEvent = () => {
 
   // 投稿イベントを受け取ったら実行
   socket.on("publishEvent", (data) => {
-
+    chatList.push(data)
   })
 }
 // #endregion
@@ -86,10 +91,10 @@ const registerSocketEvent = () => {
     <h1 class="text-h3 font-weight-medium">Vue.js Chat チャットルーム</h1>
     <div class="mt-10">
       <p>ログインユーザ：{{ userName }}さん</p>
-      <textarea variant="outlined" placeholder="投稿文を入力してください" rows="4" class="area"></textarea>
+      <textarea variant="outlined" placeholder="投稿文を入力してください" rows="4" class="area" v-model="chatContent"></textarea>
       <div class="mt-5">
-        <button class="button-normal">投稿</button>
-        <button class="button-normal util-ml-8px">メモ</button>
+        <button class="button-normal" @click="onPublish">投稿</button>
+        <button class="button-normal util-ml-8px" @click="onMemo">メモ</button>
       </div>
       <div class="mt-5" v-if="chatList.length !== 0">
         <ul>
