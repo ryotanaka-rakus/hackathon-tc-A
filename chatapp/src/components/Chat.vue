002804a41c7d73e97ed394ef1907e14d6137ec16
K4-R4 <mabdman512@gmail.com>
2023-11-09 17:08:50 +0900

    不要なハンドラを削除

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -88,21 +88,6 @@ const onReceiveMemo = (memo) => {
 // #endregion
 
 // #region socket event handler
-// サーバから受信した入室メッセージ画面上に表示する
-const onReceiveEnter = (data) => {
-  chatList.unshift(data)
-}
-
-// サーバから受信した退室メッセージを受け取り画面上に表示する
-const onReceiveExit = (data) => {
-  chatList.unshift(data)
-}
-
-// サーバから受信した投稿メッセージを画面上に表示する
-const onReceivePublish = (data) => {
-  chatList.unshift(data)
-}
-
 // サーバーから受信したエラーを処理する
 const onReceiveError = (errorMessage) => {
   // エラーメッセージをどのように表示するかは、アプリケーションに依存します
