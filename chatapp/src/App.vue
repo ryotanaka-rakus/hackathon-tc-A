12bd181c1c510e1cb773c76e91b55802607741b2
Hikoyann <i21i022dl@ous.jp>
2023-11-11 16:32:56 +0900

    ユーザー名の表示をログイン画面では見えないようにしました。

--- chatapp/src/App.vue
+++ chatapp/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { provide, ref } from "vue"
+import { computed, onMounted, provide, ref } from "vue"
+import { useRoute } from "vue-router";
 
 // #region reactive state
 const userName = ref("")
@@ -8,13 +9,20 @@ const userName = ref("")
 // #region global variable
 provide("userName", userName)
 // #endregion
+
+const route = useRoute();
+const isUserNameVisible = computed(() => {
+  return route.path !== "/";
+});
+
+
 </script>
 
 <template>
   <header>
     <div class="border-b-2 border-gray-500 flex flex-row items-center justify-between">
       <h1 class="text-blue-700 font-bold text-3xl mb-2 max-w-xl">ChatApp</h1>
-      <div class="max-w-xl"><p>ユーザー名：{{ userName }}</p></div>
+      <div class="max-w-xl"><p v-if="isUserNameVisible">ユーザー名：{{ userName }}</p></div>
     </div>
   </header>
   <router-view />
