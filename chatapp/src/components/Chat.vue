e527d08622ed4a7a6af980a4f942b73593e4aeb5
Hikoyann <i21i022dl@ous.jp>
2023-11-14 12:03:51 +0900

    pullした際にデザインが消えていたため、微調整

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -108,8 +108,6 @@ const fetchUserBookmarks = () => {
 // #endregion
 
 // #region socket event handler
-<<<<<<< Updated upstream
-=======
 // サーバから受信した入室メッセージ画面上に表示する
 const onReceiveEnter = (data) => {
   chatList.unshift(data)
@@ -127,7 +125,6 @@ const onReceivePublish = (data) => {
   chatList.unshift(data)
 }
 
->>>>>>> Stashed changes
 // サーバーから受信したエラーを処理する
 const onReceiveError = (errorMessage) => {
   // エラーメッセージをどのように表示するかは、アプリケーションに依存します
@@ -151,12 +148,9 @@ const registerSocketEvent = () => {
 
   // 投稿イベントを受け取ったら実行
   socket.on("publishEvent", (data) => {
-<<<<<<< Updated upstream
-    chatList.push(data)
-=======
     console.log(data)
     chatList.unshift(data)
->>>>>>> Stashed changes
+
   })
 
   // メモイベントを受け取ったら実行
@@ -170,7 +164,6 @@ const registerSocketEvent = () => {
     userList.splice(0, userList.length, ...users) // 受け取ったユーザー一覧で更新
   })
 
-<<<<<<< Updated upstream
   // ブックマーク保存イベントを受け取ったら実行
   socket.on("bookmarkSavedEvent", (bookmark) => {
     bookmarkList.push(bookmark);
@@ -186,19 +179,16 @@ const registerSocketEvent = () => {
   socket.on("roomChatListEvent", (receivedCharList) => {
     chatList.splice(0, chatList.length, ...receivedCharList)
   });
-=======
 
 };
 const formatTimestamp = (timestamp) => {
   timestamp = new Date();
   return timestamp.toLocaleString();
->>>>>>> Stashed changes
 }
 
 </script>
 
 <template>
-<<<<<<< Updated upstream
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
   <div class="flex">
     <div class="w-64 md:w-72 border-r-2 border-gray-500 p-2 flex flex-col h-auto">
@@ -275,25 +265,6 @@ const formatTimestamp = (timestamp) => {
             <button class="ml-5 py-2 px-3 bg-gray-500 rounded text-white hover:bg-blue-500 shadow-lg transition-all hover:shadow-lg hover:shadow-blue-500/25 focus:outline-none focus:border-blue-300" @click="onMemo">メモ</button>
           </div>
         </div>
-=======
-  <div class="mx-auto my-5 px-4">
-    <h1 class="text-h3 font-weight-medium">Vue.js Chat チャットルーム</h1>
-    <div class="mt-10">
-      <p>ログインユーザ：{{ userName }}さん</p>
-      <textarea variant="outlined" placeholder="投稿文を入力してください" rows="4" class="area" v-model="chatContent"
-        ></textarea>
-      <div class="mt-5">
-        <button class="button-normal" @click="onPublish">投稿</button>
-        <button class="button-normal util-ml-8px" @click="onMemo">メモ</button>
-      </div>
-      <div class="mt-5" v-if="chatList.length !== 0">
-        <ul>
-          <li class="item mt-4" v-for="(chat, i) in chatList" :key="i">
-            <span v-html="userList.filter((user) => user.id == chat.senderId)[0].name + 'さん: ' + chat.content"></span>
-            <span class="message-timestamp">{{ formatTimestamp(chat.timestamp) }}</span>
-          </li>
-        </ul>
->>>>>>> Stashed changes
       </div>
     </div>
   </div>
