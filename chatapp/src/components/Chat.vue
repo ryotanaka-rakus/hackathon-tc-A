56d1bae3735168ea74ad6d82528c6339076f8d87
Hikoyann <i21i022dl@ous.jp>
2023-11-14 13:38:54 +0900

    styleのコードを削除

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -258,8 +258,6 @@ const formatTimestamp = (timestamp) => {
         </div>
         <div class="flex justify-center mt-5">
           <textarea variant="outlined" placeholder="投稿文を入力してください" class="pr-0 border-2 border-solid border-gray-300 w-96" v-model="chatContent"></textarea>
-          
-            <!-- <textarea variant="outlined" placeholder="投稿文を入力してください" class="border-2 border-solid border-gray-400 pr-0 w-full" v-model="chatContent"></textarea> -->
           <div class="mt-2 ml-5">
             <button class="py-2 px-3 bg-gray-500 rounded text-white hover:bg-blue-500 shadow-lg transition-all hover:shadow-lg hover:shadow-blue-500/25 focus:outline-none focus:border-blue-300" @click="onPublish">投稿</button>
             <button class="ml-5 py-2 px-3 bg-gray-500 rounded text-white hover:bg-blue-500 shadow-lg transition-all hover:shadow-lg hover:shadow-blue-500/25 focus:outline-none focus:border-blue-300" @click="onMemo">メモ</button>
@@ -269,34 +267,3 @@ const formatTimestamp = (timestamp) => {
     </div>
   </div>
 </template>
-
-<style scoped>
-.link {
-  text-decoration: none;
-}
-
-.area {
-  width: 500px;
-  border: 1px solid #000;
-  margin-top: 8px;
-}
-
-.item {
-  display: block;
-}
-
-.util-ml-8px {
-  margin-left: 8px;
-}
-
-.button-exit {
-  color: #000;
-  margin-top: 8px;
-}
-
-.message-timestamp {
-  font-size: 0.8em;
-  color: #777;
-  margin-left: 10px;
-}
-</style>
